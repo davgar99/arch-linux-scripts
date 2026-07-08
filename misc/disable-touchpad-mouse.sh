af6933c0d572a7321f974caad32d88fc4988a6c9
David E. Garcia <davidgarcia117@gmail.com>
2026-07-08 13:21:41 -0400

    Add script to disable PlayStation controller touchpads from acting as a mouse

---
+++ misc/disable-touchpad-mouse.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Don't allow users to run this script as root
+if ((EUID == 0)); then
+    echo "This script should not be run as root. Please run it as a regular user."
+    exit 1
+fi
+
+echo "Setting udev rules to disable PlayStation 4 and PlayStation 5 controller touchpads from acting as a mouse."
+cat << 'EOF' | sudo tee -a /etc/udev/rules.d/72-dualshock4_touchpad.rules > /dev/null
+# Disable PlayStation 4 DualShock 4 touchpad acting as mouse
+# USB
+ATTRS{name}=="Sony Interactive Entertainment Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+# Bluetooth
+ATTRS{name}=="Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+EOF
+
+cat << 'EOF' | sudo tee -a /etc/udev/rules.d/72-dualsense_touchpad.rules > /dev/null
+# Disable PlayStation 5 DualSense touchpad acting as mouse
+# USB
+ATTRS{name}=="Sony Interactive Entertainment DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+# Bluetooth
+ATTRS{name}=="DualSense Wireless Controller Touchpad", ENV{LIBINPUT_IGNORE_DEVICE}="1"
+EOF
